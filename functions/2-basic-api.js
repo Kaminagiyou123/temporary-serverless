8c1f011771fa221bf12997eb1f34012e8065c650
Ran You <ran.you8311@gmail.com>
2021-01-30 15:03:38 -0800

    fix error

--- functions/2-basic-api.js
+++ functions/2-basic-api.js
@@ -2,6 +2,9 @@ const items=require('../assets/data')
 
 exports.handler= async (event,context,cb)=>{
     return {
+headers:{
+    'Access-Control-Allow-Orgin':'*',
+},
 statusCode:200,
 body: JSON.stringify(items),
     }
